981cba386e90f9a00e144bd3f0b43f21ed055de8
kjh42447 <kjh42447@gmail.com>
2022-12-16 15:38:25 +0900

    백준 11058 크리보드

---
+++ 백준/java/BJ_11058.java
@@ -0,0 +1,29 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Main {
+//    static int N;
+//    static long[] dp;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        N = Integer.parseInt(br.readLine());
+//        dp = new long[N+1];
+//        for(int i = 1; i <= N; i++) {
+//            dp[i] = i;
+//        }
+//
+//        for (int i = 5; i < N+1; i++) {
+//            dp[i] = i;
+//            for (int j = i-5; j < i-2; j++) {
+//                long tmp = dp[j] * (i-j-1);
+//                if (dp[i] < tmp) {
+//                    dp[i] = tmp;
+//                }
+//            }
+//        }
+//
+//        System.out.println(dp[N]);
+//    }
+//}
--- 백준/java/Main.java
+++ 백준/java/Main.java
@@ -1,66 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     static int N;
-    static String[] charList;
-    static int[] numCheck = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-    static int[] numList;
-    static boolean isFirst = false;
-    static String minNum;
-    static String maxNum;
+    static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        numList = new int[N+1];
-        charList = br.readLine().split(" ");
-
-        func(0);
-        System.out.println(maxNum);
-        System.out.println(minNum);
-    }
-
-    public static void func(int idx) {
-        if(idx > N) {
-            if (!isFirst) {
-                minNum = Arrays.toString(numList).replaceAll("[^0-9]","");
-                isFirst = true;
-            }
-            maxNum = Arrays.toString(numList).replaceAll("[^0-9]","");
-            return;
+        dp = new long[N+1];
+        for(int i = 1; i <= N; i++) {
+            dp[i] = i;
         }
 
-        for (int i = 0; i < 10; i++) {
-            if (numCheck[i] == -1) {
-                if(idx == 0) {
-                    numList[idx] = i;
-                    numCheck[i] = idx;
-                    func(idx + 1);
-                    numList[idx] = 0;
-                    numCheck[i] = -1;
-                }
-                else {
-                    if (isCorrect(numList[idx-1], i, charList[idx-1])) {
-                        numList[idx] = i;
-                        numCheck[i] = idx;
-                        func(idx + 1);
-                        numList[idx] = 0;
-                        numCheck[i] = -1;
-                    }
+        for (int i = 5; i < N+1; i++) {
+            dp[i] = i;
+            for (int j = i-5; j < i-2; j++) {
+                long tmp = dp[j] * (i-j-1);
+                if (dp[i] < tmp) {
+                    dp[i] = tmp;
                 }
             }
         }
-    }
 
-    public static boolean isCorrect(int num1, int num2, String op) {
-        if(op.equals("<")) {
-            return num1 < num2;
-        }
-        else {
-            return num1 > num2;
-        }
+        System.out.println(dp[N]);
     }
 }
